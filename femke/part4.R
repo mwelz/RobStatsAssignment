9aa8f13f007bd5dc94d264b73225b90730a4ad2c
r0628789 <fffeeemmmkkkeee@hotmail.com>
2021-03-27 10:57:31 +0100

    27M_creating_data

--- femke/part4.R
+++ femke/part4.R
@@ -1,25 +1,28 @@
+install.packages("energy")
+library(energy)
 # Part 4 # Femke
-
-# 1 degree of freedom -----------------------------------------------------
-## Jakob Raymaekers Cauchy distribution
-
-# Figure 8 in section 5.3 in [2] carries out a different experiment, where the power of
-# the permutation test implemented in dcor.test is plotted as a function of the sample
-# size n (left) and the dimension d (right).
-
-# 2 degrees of freedom ----------------------------------------------------
-## Analogue student t-distibutions 2 degrees of freedom
-
+# significance level
+alpha = 0.1
 # number of replication keep m < 1000 
 m = 3 # trial run
 # sample size
-n = seq(10,80,by=10)
+n = 10 #seq(10,80,by=10)
+# dimension
+d = 2 #seq(1,30,by=1)
 
+#counter=0
+#for(j in m){
+X=zeros(d,n)
+Y=zeros(d,n)
+X[1]= rt(n,df=2)
+Y[1]=X[1]
 # sample of student t distribution df=2
-sample = rt(n,df=2)  #ncp non-centrality parameter omitted => central t distribution
+for(i in 2:d){
+  X[i]= rt(n,df=2)
+  Y[i]= rt(n,df=2)
+}
 
-# significance level
-alpha = 0.1
+
 
 # running dcor on sample
 counter=0
