a9c9aa3d16428feaea069661a40ba8cf4c802369
r0628789 <fffeeemmmkkkeee@hotmail.com>
2021-03-27 12:07:53 +0100

    27M creating data, failed dcor.test

--- femke/part4.R
+++ femke/part4.R
@@ -1,32 +1,53 @@
 install.packages("energy")
 library(energy)
 # Part 4 # Femke
+
 # significance level
 alpha = 0.1
 # number of replication keep m < 1000 
-m = 3 # trial run
+m = 3 # trial run 
+
+
+
+
+# left --------------------------------------------------------------------
 # sample size
-n = 10 #seq(10,80,by=10)
+n = seq(10,80,by=10)
 # dimension
-d = 2 #seq(1,30,by=1)
-
-#counter=0
-#for(j in m){
-X=zeros(d,n)
-Y=zeros(d,n)
-X[1]= rt(n,df=2)
-Y[1]=X[1]
-# sample of student t distribution df=2
-for(i in 2:d){
-  X[i]= rt(n,df=2)
-  Y[i]= rt(n,df=2)
-}
+d = 2
 
 
+results=matrix(0,length(n),m)
+# results=rep(0,k)
+# loop over sample size
+for(k in 1:8){
+  # loop over replications
+  for(j in 1:m){
+    #create data for specific setting
+    # samples of student t distribution df=2
+    X=matrix(0,d,n[k])
+    X[1,]= rt(n[k],df=2)
+    Y=X
+    for(i in 2:d){
+      X[i,]= rt(n[k],df=2)
+      Y[i,]= rt(n[k],df=2)
+    }
+    result=dcor.test(X,Y,m) 
+    # asks for number of replications?
+    # pvalue = NA? 
+    # dcor(X,Y)
+    results[k,j]=result[["p.value"]]
+    
+    # if(result<alpha){
+    # counter=counter+1
+    # }
+  }
+  # # determine fraction rejecting the null hypothesis
+  # empow=counter/n
+  # results[k]= empow
+  }
 
-# running dcor on sample
-counter=0
-# determin fraction rejecting the null hypothesis
-empow=counter/n
-# plotting in function of sample size
+
+
+# plotting empow in function of sample size
 # and in function of dimension
