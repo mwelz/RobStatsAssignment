873ae12b0893dcf537e8e3823d2efbbdfe420733
r0628789 <fffeeemmmkkkeee@hotmail.com>
2021-03-30 23:34:40 +0200

    femke_30M_All_Ones_:(

--- femke/part4.R
+++ femke/part4.R
@@ -1,4 +1,4 @@
-install.packages("energy")
+# install.packages("energy")
 library(energy)
 # Part 4 # Femke
 
@@ -17,13 +17,13 @@ n = seq(10,80,by=10)
 d = 2
 
 
-results=matrix(0,length(n),m)
-# results=rep(0,k)
+#results=matrix(0,length(n),m)
+results=rep(0,k)
 # loop over sample size
 for(k in 1:8){
   # loop over replications
-  for(j in 1:m){
-    #create data for specific setting
+  # for(j in 1:m){
+    # create data for specific setting
     # samples of student t distribution df=2
     X=matrix(0,d,n[k])
     X[1,]= rt(n[k],df=2)
@@ -32,22 +32,28 @@ for(k in 1:8){
       X[i,]= rt(n[k],df=2)
       Y[i,]= rt(n[k],df=2)
     }
-    result=dcor.test(X,Y,m) 
+    result=dcor.test(dist(X),dist(Y),R=m) 
     # asks for number of replications?
     # pvalue = NA? 
     # dcor(X,Y)
-    results[k,j]=result[["p.value"]]
+    results[k]=result[["p.value"]]
     
     # if(result<alpha){
     # counter=counter+1
     # }
-  }
+  #}
   # # determine fraction rejecting the null hypothesis
   # empow=counter/n
   # results[k]= empow
-  }
+}
 
 
 
 # plotting empow in function of sample size
 # and in function of dimension
+
+# Try something different --------------------------------------------------
+X=rt(10,2)
+Y=rt(10,2)
+dcor(X,Y)
+dcor.test(X,Y,1,5)
