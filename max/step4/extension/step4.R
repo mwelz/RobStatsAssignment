707334787302c74b1a2baf7fd34011af2ca15282
mwelz <welz@ese.eur.nl>
2021-04-01 20:44:50 +0200

    Added replication of Figure 8 in the paper and fixed an error in the plots (plots erroneously used to show the p-values rather than the power)

---
+++ max/step4/extension/step4.R
@@ -0,0 +1,24 @@
+#' ----------------------------------------------------------------
+#' Revise Figure 8 in the second paper with different settings 
+#' 
+#' Author: mwelz & femke
+#' Last changed: Apr 1, 2021
+#' ----------------------------------------------------------------
+rm(list = ls()) ; gc() ; cat("\014") 
+
+
+## 0. initialize ----
+# load helper functions
+source(paste0(getwd(), "/max/step4/extension/step4-functions.R"))
+
+# initialize simulation parameters
+n.set <- seq(from = 10, to = 80, by = 10) 
+d.set <- 1:30 
+m     <- 1000
+alpha <- 0.1 # significance level
+
+# simulate
+tests.ls <- simulate(n.set = n.set, d.set = d.set, m = m, seed = 1, df = 2)
+
+# store
+save(tests.ls, file = paste0(getwd(), "/max/step4/extension/step4-simdata.Rdata"))
